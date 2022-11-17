29e67a5d3f483d1232ab8907cd2313254796a852
Harrison K <53415491+kungharrison@users.noreply.github.com>
2022-11-17 09:21:29 -0800

    expose & explore

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Lab 8 - Starter
+Created by Harrison Kung
+
+1. Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+    - Within a Github actino that runs whenever code is published, to help with
+    automation and to make tests consistently run for all developers.
+2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+    - No, a function is only a small part of the program, and a full end-to-end test
+    would be too slow and too expensive to run for every function.
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+    - No, a unit test would not be able to test the message feature, as it would
+    require a user to send a message to another user, which is not possible in a
+    unit test.
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+    - Yes, a unit test would be able to test the max message length feature, as it
+    would only require the user to type more than 80 characters, which is possible
+    in a unit test.
+
+![Lab 8 Test](lab8test.png)
+![Sum Test](sumtest.png)
+![Unit Test](unittest.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,73 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// isPhoneNumber tests
+test('isPhoneNumber returns true', () => {
+    expect(functions.isPhoneNumber('555-555-5555')).toBe(true);
+});
+test('isPhoneNumber returns true', () => {
+    expect(functions.isPhoneNumber('(555) 555-5555')).toBe(true);
+});
+test('isPhoneNumber returns false', () => {
+    expect(functions.isPhoneNumber('422-392-848')).toBe(false);
+});
+test('isPhoneNumber returns false', () => {
+    expect(functions.isPhoneNumber('912939393933')).toBe(false);
+});
+
+// isEmail tests
+test('isEmail returns true', () => {
+    expect(functions.isEmail('lanejones@gmail.com')).toBe(true);
+});
+test('isEmail returns true', () => {
+    expect(functions.isEmail('ewt38s0@hotmail.com')).toBe(true);
+});
+test('isEmail returns false', () => {
+    expect(functions.isEmail('sdi3j.sdjej2480')).toBe(false);
+});
+test('isEmail returns false', () => {
+    expect(functions.isEmail('Ewt.@..gma...il.com')).toBe(false);
+});
+
+// isStrongPassword tests
+test('isStrongPassword returns true', () => {
+    expect(functions.isStrongPassword('csdueusj')).toBe(true);
+});
+test('isStrongPassword returns true', () => {
+    expect(functions.isStrongPassword('applePassword5')).toBe(true);
+});
+test('isStrongPassword returns false', () => {
+    expect(functions.isStrongPassword('11111111111111111111111')).toBe(false);
+});
+test('isStrongPassword returns false', () => {
+    expect(functions.isStrongPassword('32422')).toBe(false);
+});
+
+// isDate tests
+test('isDate returns true', () => {
+    expect(functions.isDate('1/3/2027')).toBe(true);
+});
+test('isDate returns true', () => {
+    expect(functions.isDate('15/24/3999')).toBe(true);
+});
+test('isDate returns false', () => {
+    expect(functions.isDate('23-38-8288')).toBe(false);
+});
+test('isDate returns false', () => {
+    expect(functions.isDate('24t08u20ug08J)*8')).toBe(false);
+});
+
+// isHexColor tests
+test('isHexColor returns true', () => {
+    expect(functions.isHexColor('fff')).toBe(true);
+});
+test('isHexColor returns true', () => {
+    expect(functions.isHexColor('e2e2e2')).toBe(true);
+});
+test('isHexColor returns false', () => {
+    expect(functions.isHexColor('asdjfwjeiwj')).toBe(false);
+});
+test('isHexColor returns false', () => {
+    expect(functions.isHexColor('993j99999(#*89h')).toBe(false);
+});
